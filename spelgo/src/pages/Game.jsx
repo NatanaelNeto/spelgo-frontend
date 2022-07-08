49a4267a367f209a5e236e6a3062c931b23e5742
Natanael Neto <natanael.eneas2018@hotmail.com>
2022-07-08 01:49:27 -0300

    ajusta selecionador de palavras

--- spelgo/src/pages/Game.jsx
+++ spelgo/src/pages/Game.jsx
@@ -8,6 +8,7 @@ class Game extends React.Component {
 
     this.state = {
       words: [],
+      currWords: [],
     };
 
     this.fetchWords = this.fetchWords.bind(this);
@@ -25,12 +26,7 @@ class Game extends React.Component {
     const data = await fetch('https://termo-crente.herokuapp.com/words', payload)
       .then(response => response.json());
     const { words } = data;
-    this.setState({ words });
-  }
-
-  render() {
-    const { active, quantity } = this.props;
-    const { words } = this.state;
+    const { quantity } = this.props;
     const PRIME = 27644437;
 
     const currDate = new Date();
@@ -39,18 +35,37 @@ class Game extends React.Component {
     const ano = currDate.getUTCFullYear();
 
     const fulldate = (parseInt("" + dia + mes + ano)) * dia * mes * ano;
-    let currwords = [];
+    const todayWords = [];
+    const currWords = [];
 
-    for (let i = 0; i < quantity && words.length > 0; i += 1) {
-      const index = (fulldate * quantity * (PRIME ** (2 * (i + 1)))) % words.length;
-      currwords.push(words[index]);
+    for (let i = 1; i <= 10; i += 1) {
+      let trial = 0;
+      let index = (fulldate * i + (PRIME * trial)) % words.length;
+      while(todayWords.includes(words[index])) {
+        trial += 1;
+        index = (fulldate * i + (PRIME * trial)) % words.length;
+      }
+      todayWords.push(words[index]);
+    } 
+
+    for (let i = 0; i < quantity; i += 1) {
+      currWords.push(todayWords[i + this.startIndex(quantity)]);
     }
 
-    currwords = currwords.filter((item, index) => currwords.indexOf(item) === index);
+    this.setState({ words, currWords });
+  }
+
+  startIndex (qtd) {
+    return ((qtd ** 2) - qtd) / 2;
+  }
+
+  render() {
+    const { active, quantity } = this.props;
+    const { currWords } = this.state;
 
     const conjuntos = [];
-    for (let i = 0; i < currwords.length; i += 1) {
-      conjuntos.push(<Conjunto key={i} qtd={ 5 + parseInt(quantity) } palavra={currwords[i]} />);
+    for (let i = 0; i < currWords.length; i += 1) {
+      conjuntos.push(<Conjunto key={i} qtd={ 5 + parseInt(quantity) } palavra={currWords[i]} />);
     }
 
     return active ?
@@ -58,7 +73,7 @@ class Game extends React.Component {
         <div>
           <Header active = {active} />
           <div className="game-content">
-            { currwords.length > 0 ? conjuntos.map((item) => item) : <h3>Carregando...</h3> }
+            { currWords.length > 0 ? conjuntos.map((item) => item) : <h3>Carregando...</h3> }
           </div>
         </div>
       ) :
